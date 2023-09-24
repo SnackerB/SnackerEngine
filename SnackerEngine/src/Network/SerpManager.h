eef739557347e3c55dc7c9a6f7288d9bc963fff9
SnackerB <snacker-b@gmx.de>
2023-09-24 13:03:43 +0200

    Started work on EventHandle functionality, Implemented NetworkManager interface.

--- SnackerEngine/src/Network/SerpManager.h
+++ SnackerEngine/src/Network/SerpManager.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "H"
+#include "SERP/SERP.h"
+#include "TCP/TCP.h"
 
 namespace SnackerEngine
 {
@@ -9,7 +10,16 @@ namespace SnackerEngine
 	/// running SnackerEngine over the SERP server
 	class SERPManager
 	{
+	private:
+		/// Wether the SERP manager is connected to the SERP server
 		bool connected = false;
+		/// The socket that is used to communicate with the server
+		SnackerNet::SocketTCP socket;
+	public:
+		/// Constructor
+		SERPManager();
+		/// Tries to connect to the SERP server
+		void connectToSERPServer();
 
 	};
 
--- SnackerEngine/src/Utility/Handles/EventHandle.h
+++ SnackerEngine/src/Utility/Handles/EventHandle.h
@@ -1,1 +1,65 @@
 #pragma once
+
+#include <set>
+
+namespace SnackerEngine
+{
+
+	class EventHandle
+	{
+	public:
+		/// Class representing an object that can be "observed" by handles
+		class Observable
+		{
+		private:
+			/// Set of pointers to all event handles that subscribed to this observable
+			std::set<EventHandle*> eventHandles;
+		protected:
+			friend class EventHandle;
+			/// Notifies the observable that the given eventHandle has moved
+			void notifyHandleMove(EventHandle* old, EventHandle& eventHandle);
+		public:
+			/// subscribes the given eventHandle to this observable
+			void subscribe(EventHandle& eventHandle);
+			/// unsubscribes the given eventHandle from this observable
+			void unsubscribe(EventHandle& eventHandle);
+			/// Triggers an event and notifies all subscribed eventHandles
+			void trigger();
+			/// deleted copy constructor and assignment operator
+			Observable(const Observable& other) = delete;
+			Observable& operator=(const Observable& other) = delete;
+			/// Move constructor and assignment operator
+			Observable(Observable&& other) noexcept;
+			Observable& operator=(Observable&& other) noexcept;
+			/// Destructor
+			~Observable();
+		};
+	private:
+		/// Set of observables that this handle is subscribed to
+		std::set<Observable*> observables{};
+		/// this is set to true if an event occurred and can be reset by calling reset()
+		bool active = false;
+		/// Helper function that is called by observables to notify the handle an
+		/// event has occured
+		void notifyEvent();
+	protected:
+		/// Function that gets called when an event happens.
+		virtual void onEvent() {}
+	public:
+		/// Unsubscribes from all observables
+		void unsubscribeFromAll();
+		/// Copy constructor and assignment operator
+		EventHandle(const EventHandle& other) noexcept;
+		EventHandle& operator=(const EventHandle& other) noexcept;
+		/// Move constructor and assignment operator
+		EventHandle(EventHandle&& other) noexcept;
+		EventHandle& operator=(EventHandle&& other) noexcept;
+		/// Destructor
+		~EventHandle();
+		/// Getters
+		bool isActive() const { return active; }
+		/// Resets the handle
+		void reset() { active = false; }
+	};
+
+}
